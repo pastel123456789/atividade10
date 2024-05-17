35db2f41d749dd71249b58826c78a9010f6924ac
pastel DeQueijo <kauan.soliveira@eaportal.org>
2024-05-17 18:36:20 +0000

    criação dos primeiros estilos

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Home from "./pages/Home"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+}
+]) 
+    
+
+
+
+
+export default function App() {
+  return (
+    <RouterProvider router={router}/>
+  )
+}
